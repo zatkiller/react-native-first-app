eb1dbc3e3f47ebe207539dd1e0c77e83191035cb
zatkiller <59414467+zatkiller@users.noreply.github.com>
2020-05-22 23:41:37 +0800

    Updating Notes and App

--- App.js
+++ App.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{ padding: 30 }}>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+        <TextInput placeholder="Course Goal" style={{
+          width: '80%', borderColor: 'black', borderWidth: 1, padding: 10
+        }} />
+        <Button title="Add" />
+      </View>
+      <View>
+
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
